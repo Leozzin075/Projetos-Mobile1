8505e041b47928e491b98cdc26a68a609ef442d8
Leozzin075 <99297345+Leozzin075@users.noreply.github.com>
2022-11-24 12:58:41 -0300

    80%

--- Quiz/app/src/main/java/com/example/quiz/jv_final.java
+++ Quiz/app/src/main/java/com/example/quiz/jv_final.java
@@ -1,15 +1,48 @@
 package com.example.quiz;
 
+import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class jv_final extends AppCompatActivity {
-
+    Button reiniciar;
+    TextView informacao;
+    ImageView resultado;
+    View view;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.tela_final);
+        reiniciar = findViewById(R.id.reiniciar);
+        informacao = findViewById(R.id.informacao);
+        resultado = findViewById(R.id.resultado);
+        view = findViewById(R.id.view);
+        Intent i = getIntent();
+        String m1 = i.getStringExtra(jv_quiz.codigo);
+        if(m1.equals("passou")){
+            view.setBackgroundResource(R.color.Green);
+            resultado.setImageResource(R.drawable.ganhou);
+            informacao.setText("Parabens voce acertou mais de 60% das perguntas");
+        }else if(m1.equals("perdeu")){
+            view.setBackgroundResource(R.color.Red);
+            resultado.setImageResource(R.drawable.perdeu);
+            informacao.setText("ihhh deu mole ein, errou mais de 60% das perguntas");
+        }
+
+        reiniciar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(getApplicationContext(), MainActivity.class);
+                startActivity(i);
+            }
+        });
+
 
     }
 }
--- Quiz/app/src/main/java/com/example/quiz/jv_quiz.java
+++ Quiz/app/src/main/java/com/example/quiz/jv_quiz.java
@@ -1,5 +1,7 @@
 package com.example.quiz;
 
+import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.os.CountDownTimer;
 import android.util.Log;
@@ -8,61 +10,133 @@ import android.widget.Button;
 import android.widget.ProgressBar;
 import android.widget.TextView;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
-public class jv_quiz extends AppCompatActivity {
+public class jv_quiz extends AppCompatActivity implements View.OnClickListener {
 
     public TextView perguntas;
-    public Button r, r1, r2, r3;
+    public Button r, r1, r2, r3, enviar;
     public ProgressBar tempo;
-    int i=0;
-    private String[] enuciados = {
+    int i = 0;
+    public static final String codigo = "123";
 
-    };
-    private String[] respostas = {
+    int score = 0;
+    int totalQuestion = questoes_jv.question.length;
+    int currentQuestionIndex = 0;
+    String selectedAnswer = "";
 
-    };
-    private int[] respostas_certas = {
-
-    };
 
     @Override
-    protected void onCreate(Bundle savedInstanceState){
-    super.onCreate(savedInstanceState);
-    setContentView(R.layout.tela_quiz);
-    perguntas = findViewById(R.id.perguntas);
-    r = findViewById(R.id.r);
-    r1 = findViewById(R.id.r1);
-    r2 = findViewById(R.id.r2);
-    r3 = findViewById(R.id.r3);
-    tempo = findViewById(R.id.tempo);
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.tela_quiz);
+        perguntas = findViewById(R.id.perguntas);
+        r = findViewById(R.id.r);
+        r1 = findViewById(R.id.r1);
+        r2 = findViewById(R.id.r2);
+        r3 = findViewById(R.id.r3);
+        enviar = findViewById(R.id.enviar);
+        tempo = findViewById(R.id.tempo);
+
+        r.setOnClickListener(this);
+        r1.setOnClickListener(this);
+        r2.setOnClickListener(this);
+        r3.setOnClickListener(this);
+        enviar.setOnClickListener(this);
 
 
-
-
-
-
+        loadNewQuestion();
 
         //Temporizador
         tempo.setProgress(i);
-        new CountDownTimer(15000, 10){
+        new CountDownTimer(15000, 10) {
 
             @Override
             public void onTick(long millisUntilFinished) {
-                Log.v("Log_tag", "Tick of Progress"+ i+ millisUntilFinished);
+                Log.v("Log_tag", "Tick of Progress" + i + millisUntilFinished);
                 i++;
-                tempo.setProgress((int)(((1.0 * millisUntilFinished) / 15000) * 100));;
+                tempo.setProgress((int) (((1.0 * millisUntilFinished) / 15000) * 100));
+                ;
             }
 
             @Override
             public void onFinish() {
-            //Descobrir como definir a resposta certa
-            //Implementar funçao de quando clicar na reposta resetar o tempo
-            //Fazer contador
+                //Descobrir como definir a resposta certa
+                //Implementar funçao de quando clicar na reposta resetar o tempo
+                //Fazer contador
 
             }
         }.start();
 
+    }
 
+    @Override
+    public void onClick(View view) {
+        r.setBackgroundColor(Color.WHITE);
+        r1.setBackgroundColor(Color.WHITE);
+        r2.setBackgroundColor(Color.WHITE);
+        r3.setBackgroundColor(Color.WHITE);
+
+        Button clickedButton = (Button) view;
+        if (clickedButton.getId() == R.id.enviar) {
+            if (selectedAnswer.equals(questoes_jv.correctAnswers[currentQuestionIndex])) {
+                score++;
+            }
+            currentQuestionIndex++;
+            loadNewQuestion();
+        } else{
+        //choices button clicked
+        selectedAnswer  = clickedButton.getText().toString();
+        clickedButton.setBackgroundColor(Color.MAGENTA);
+        }
+    }
+    void loadNewQuestion(){
+
+        if(currentQuestionIndex == totalQuestion ){
+            finishQuiz();
+            return;
+        }
+
+        perguntas.setText(questoes_jv.question[currentQuestionIndex]);
+        r.setText(questoes_jv.choices[currentQuestionIndex][0]);
+        r1.setText(questoes_jv.choices[currentQuestionIndex][1]);
+        r2.setText(questoes_jv.choices[currentQuestionIndex][2]);
+        r3.setText(questoes_jv.choices[currentQuestionIndex][3]);
+
+    }
+
+    void finishQuiz(){
+        if(score > totalQuestion*0.60){
+//            passStatus = "Passed";
+            Intent sgd;
+            String m1 = "passou";
+            sgd = new Intent(getApplicationContext(), jv_final.class);
+            sgd.putExtra(codigo, m1);
+            startActivity(sgd);
+        }else{
+//            passStatus = "Failed";
+            Intent sgd;
+            String m1 = "perdeu";
+            sgd = new Intent(getApplicationContext(), jv_final.class);
+            sgd.putExtra(codigo, m1);
+            startActivity(sgd);
+
+        }
+
+//        new AlertDialog.Builder(this)
+//                .setTitle(passStatus)
+//                .setMessage("Score is "+ score+" out of "+ totalQuestion)
+//                .setPositiveButton("Restart",(dialogInterface, i) -> restartQuiz() )
+//                .setCancelable(false)
+//                .show();
+
+
+    }
+
+    void restartQuiz(){
+        score = 0;
+        currentQuestionIndex =0;
+        loadNewQuestion();
     }
 }
